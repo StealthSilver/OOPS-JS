d3084d68e83604409274d5ff986ace182854da6f
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2024-12-04 21:57:39 +0530

    init

---
+++ 01.js
@@ -0,0 +1,26 @@
+const stu1 = {
+    name: "pogo",
+    age: 2,
+    marks: 67,
+    getMarks: function () {
+        return this.marks
+    }
+}
+
+const stu2 = {
+    name: "groot",
+    age: 32,
+    marks: 5,
+    getMarks: function () {
+        return this.marks
+    }
+}
+
+const stu3 = {
+    name: "captain",
+    age: 56,
+    marks: 62,
+    getMarks: function () {
+        return this.marks
+    }
+}
